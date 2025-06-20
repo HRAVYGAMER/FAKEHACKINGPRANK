ea47fcfa8526444d3c4e7db4a56095e2dc7d5d96
HRAVYGAMER <realsciler@gmail.com>
2025-06-20 11:02:22 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+its just a prank 
+import time
+
+print("Připojování k cíli...")
+time.sleep(1)
+print("Bypassing firewall...")
+time.sleep(2)
+print("Získávám hesla...")
+time.sleep(1.5)
+print("Heslo nalezeno: admin123")
+time.sleep(1)
+print("\n✅ HACK COMPLETED!")
